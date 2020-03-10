d507af2b34aa028aded812538df3c5b7eb626392
addiefancensie <addiefancensie@gmail.com>
2020-03-10 01:16:51 -0700

    10mar

---
+++ 10march.py
@@ -0,0 +1,24 @@
+x = "Python seems fun" #declaring a string variable x
+print(type(x)) #displaying the data type of string
+print(x[2]) #displaying second position
+print(x[5]) #displaying fifth position
+print(x[2:5]) #displaying position 2-5
+
+intNumber1 = 5 #declaring intNumber1 as 5
+intNumber2 = 5.8 #declaring intNumber2 as 5.8
+print(type(intNumber1),type(intNumber2)) #displaying the data type
+
+myList = [23,82,42,84,33] #displaying a list
+print(type(myList)) #displaying data type
+myList.insert(4,21) #insert no. 21 in the 4th posiion
+print(myList) #no. 21 iserted in 4th position
+myList.reverse #declaring a reverse in the list
+print(myList) #list reads in reverse
+myList.remove(82) #instruction to remove no. 82
+print(myList) #no. 82 removed
+myList.sort() #instruction to ascend no.s
+print(myList) #no.s are placed in ascending order
+cars = {"Ashton Martin","VW","Ford","BMW","Nissan"}
+print(type(cars)) #displaying data type
+cars1 = ("Ashton Martin","VW","Ford","BMW","Nissan")
+print(type(cars1))
